564265b800e2fe83cdc54ece6a90497410a3a7db
Ricardo Silva Flores <purpleknight170@gmail.com>
2024-04-27 17:32:06 -0300

    área de login

--- projeto_restaurante/jdbc/src/Login.java
+++ projeto_restaurante/jdbc/src/Login.java
@@ -4,22 +4,42 @@ import java.util.Map;
 // Classe para Fazer Login como Garçom ou Administrador
 public class Login {
     
-    private Map<String, String> credenciais;
+    // Usando HashMap para realizar o Login dos Usuários
+    private Map<String, String> credenciaisGarcom;
+    private Map<String, String> credenciaisAdmin;
+
+    public void LoginOpcoes(){
+        System.out.println("----------Login----------");
+        System.out.println("Insira '1' para Garçom");
+        System.out.println("Insira '2' para Administrador");
+        System.out.printf("--> ");
+    }
 
     // Definindo credenciais de Login para Garçom e administrador
     public Login(){
-        credenciais = new HashMap<>();                                // instânciando
-        credenciais.put("garcom", "garcom123");             // Definindo um usuário e senha para login
+        credenciaisGarcom = new HashMap<>();                                // instânciando Usuários de Garçom
+        credenciaisAdmin = new HashMap<>();                                 // instânciando Usuários de Admin
+        credenciaisGarcom.put("garcom", "garcom123");             // Definindo um Usuário e Senha para Garçom
+        credenciaisAdmin.put("admin", "admin123");                // Definindo um Usuário e Senha para Admin
     }
 
     // Classe para Realizar a Verificação de Login do Usuário
     public boolean LoginGarcom(String GarcomUsuario, String GarcomSenha){
-        if (credenciais == null) {
-            System.out.println("Erro: Mapa de credenciais não foi inicializado corretamente.");
-            return false;
+        // Verifica se a entrada do Usuário se iguala ao Usuário e Senha e Retorna True ou False
+        if (credenciaisGarcom.containsKey(GarcomUsuario) && credenciaisGarcom.get(GarcomUsuario).equals(GarcomSenha)) {
+            return true;
         }else{
-            // Boolean para credenciais inseridas se estão corretas ou não
-            return credenciais.containsKey(GarcomUsuario) && credenciais.get(GarcomUsuario).equals(GarcomSenha);
+            return false;
+        }
+    }
+
+    // Classe para Realizar a Verificação de Login do Admin
+    public boolean LoginAdmin(String AdminUsuario, String AdminSenha){
+        // Verifica se a entrada do Usuário se iguala ao Usuário e Senha e Retorna True ou False
+        if (credenciaisAdmin.containsKey(AdminUsuario) && credenciaisAdmin.get(AdminUsuario).equals(AdminSenha)) {
+            return true;
+        }else{
+            return false;
         }
     }
 }
