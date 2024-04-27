3771eac96098f9ca03a89815b3086910ffca43f6
Ricardo Silva Flores <purpleknight170@gmail.com>
2024-04-27 17:48:21 -0300

    area admin

--- projeto_restaurante/jdbc/src/Admin.java
+++ projeto_restaurante/jdbc/src/Admin.java
@@ -2,7 +2,8 @@ import java.sql.*;
 
 public class Admin {
     int id = 0;
-
+    
+    // Método para exibir opções de Admin
     public void MenuAdmin(){
         System.out.println("\n----------Opções----------");
         System.out.println("1: Cadastrar Pratos");
@@ -25,6 +26,22 @@ public class Admin {
             int LinhasAfetadas = stmt.executeUpdate();
             System.out.println("Linhas Afetadas: "+ LinhasAfetadas);
         }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void DeletarPratos(String nomePrato){
+        String sql = "DELETE FROM prato WHERE nome = ?";
+
+        try (Connection conexao = conectiondb.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql)){
+
+            stmt.setString(1, nomePrato);
+
+            int LinhasAfetadas = stmt.executeUpdate();
+            System.out.println("Linhas Afetadas: " + LinhasAfetadas);
+            
+        }catch (SQLException e) {
             e.printStackTrace();
         }
     }
