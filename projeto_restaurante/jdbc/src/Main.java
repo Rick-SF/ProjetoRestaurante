605b049c682d5263f6157ced14c5a892b91fa61b
Ricardo Silva Flores <purpleknight170@gmail.com>
2024-04-27 17:32:15 -0300

    área main

--- projeto_restaurante/jdbc/src/Main.java
+++ projeto_restaurante/jdbc/src/Main.java
@@ -1,30 +1,66 @@
-import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Scanner leitor = new Scanner(System.in);            // Cria um Scanner
-        Login login = new Login();
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);            // Cria Scanner
+        Login login = new Login();                          // Instância da Classe Login
+        Admin admin = new Admin();                          // Instância da Classe Admin
 
-        System.out.println("----------Login----------");
-        System.out.println("Insira '1' para Garçom");
-        System.out.println("Insira '2' para Administrador");
-        System.out.printf("-->: ");
+        // Exibe as Opções de Login
+        login.LoginOpcoes();
 
         int opcao = leitor.nextInt();
-        leitor.nextLine();
+        leitor.nextLine();                   // Limpa o Buffer
 
-        if (opcao == 1) {
-            System.out.printf("\nUsuário: ");
-            String GarcomUsuario = leitor.nextLine();
-            System.out.printf("\nSenha: ");
-            String GarcomSenha = leitor.nextLine();
+        switch (opcao) {
+            case 1:
+                System.out.printf("\nUsuário: ");       // Usuário insere o Usuário de Garçom
+                String GarcomUsuario = leitor.nextLine();      // Armazena a entrada Usuário
 
-            if(login.LoginGarcom(GarcomUsuario, GarcomSenha)){
-                System.out.println("Login Feito!");
-            }else{
-                System.out.println("Login Inválido.");
-            }
+                System.out.printf("Senha: ");           // Usuário insere a Senha de Garçom
+                String GarcomSenha = leitor.nextLine();        // Armazena a entrada Senha
+
+                if (login.LoginGarcom(GarcomUsuario, GarcomSenha)) {    // Faz a Verificação de Usuário e senha pelo Método LoginGarcom
+                    System.out.println("Login de Garçom Feito!");             // Retornando True, o Login é confirmado.
+                } else {
+                    System.out.println("Login Inválido.");          // Retornando False, o Login é impedido
+                }
+                break;
+            case 2:
+                System.out.printf("\nUsuário: ");       // Usuário Insere o Usuário de Admin
+                String AdminUsuario = leitor.nextLine();       // Armazena a entrada Usuário
+
+                System.out.printf("Senha: ");           // Usuário Insere a Senha de Admin
+                String AdminSenha = leitor.nextLine();         // Armazena a entrada Senha
+
+                if (login.LoginAdmin(AdminUsuario, AdminSenha)) {   // Faz a Verificação de Usuário e senha pelo Método LoginAdmin
+                    System.out.println("Login de Admin Feito!");        // Retornando True, o Login é confirmado e procede
+                    admin.MenuAdmin();                              // Exibe o Menu de Admin
+                    int adminopcao = leitor.nextInt();
+
+                    leitor.nextLine();                          // Limpa o Buffer
+
+                    switch (adminopcao) {
+                        case 1:
+                            System.out.printf("Nome do Prato: ");
+                            String nomePrato = leitor.nextLine();
+                            System.out.printf("Valor do Prato: ");
+                            double valorPrato = leitor.nextDouble();
+
+                            leitor.nextLine();                              // Limpa o Buffer
+
+                            admin.InserirPratos(nomePrato, valorPrato);     // Adiciona um novo prato com os parâmetros informados
+                            break;
+                        default:
+                            break;
+                    }
+                
+                } else {
+                    System.out.println("Login Inválido.");              // Retornando False, o Login é impedido.
+                }
+                break;
+            default:
+                System.out.println("Opção Inválida.");   // Mensagem de erro para opção inválida
         }
 
         leitor.close();
