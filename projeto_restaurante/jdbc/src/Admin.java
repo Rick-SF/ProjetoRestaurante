0de99e031f38b53d0244bd8dcb3076a7eb31eab4
Ricardo Silva Flores <purpleknight170@gmail.com>
2024-04-27 17:31:35 -0300

    àrea admin

---
+++ projeto_restaurante/jdbc/src/Admin.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class Admin {
+    int id = 0;
+
+    public void MenuAdmin(){
+        System.out.println("\n----------Opções----------");
+        System.out.println("1: Cadastrar Pratos");
+        System.out.println("2: Atualizar Pratos");
+        System.out.println("3: Deletar Pratos");
+        System.out.printf("--> ");
+    }
+    
+    public void InserirPratos(String nomePrato, double valorPrato){
+        String sql = "INSERT INTO prato (id, nome, valor, disponivel) VALUES (?, ?, ?, ?)";
+    
+        try (Connection conexao = conectiondb.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql)){
+
+            stmt.setInt(1, ++id);
+            stmt.setString(2, nomePrato);
+            stmt.setDouble(3, valorPrato);
+            stmt.setString(4, "1");
+
+            int LinhasAfetadas = stmt.executeUpdate();
+            System.out.println("Linhas Afetadas: "+ LinhasAfetadas);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
