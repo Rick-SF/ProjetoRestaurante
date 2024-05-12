c7dfc2ae003fc618c529dceebde7f633c4fa0eed
Ricardo Silva Flores <purpleknight170@gmail.com>
2024-05-12 11:59:12 -0300

    Novo método
    ajustes e novo método para verificação de existência de prato no banco.

--- projeto_restaurante/jdbc/src/Admin.java
+++ projeto_restaurante/jdbc/src/Admin.java
@@ -11,8 +11,9 @@ public class Admin {
         System.out.println("\n----------Opções de Admin----------");
         System.out.println("1: Cadastrar Pratos");
         System.out.println("2: Atualizar Pratos");
-        System.out.println("3: Deletar Pratos");
-        System.out.println("4: Logout");
+        System.out.println("3: Pratos Cadastrados");
+        System.out.println("4: Deletar Pratos");
+        System.out.println("5: Logout");
         System.out.printf("--> ");
     }
     
@@ -25,7 +26,7 @@ public class Admin {
 
             stmt.setString(1, nomePrato);
             stmt.setDouble(2, valorPrato);
-            stmt.setString(3, "1");
+            stmt.setString(3, "sim");
 
             int LinhasAfetadas = stmt.executeUpdate();
             System.out.println("Prato "+ nomePrato +" pelo valor de R$"+ valorPrato +" cadastrado.");
@@ -38,11 +39,20 @@ public class Admin {
     // Método para atualizar pratos existentes
     public void AtualizarPratos(int AtualiOpcao){
         switch (AtualiOpcao) {
-            case 1:
+            case 1: // Mudar nome de um Prato
                 System.out.printf("Prato no qual deseja mudar o nome: ");
                 String antigoNome = leitor.nextLine();
                 System.out.printf("Insira o Novo Nome: ");
                 String novoNome = leitor.nextLine();
+
+                String nomePrato = antigoNome;
+                if(VerificarPrato(nomePrato)){         // Verifica se o prato existe
+                    // Se sim, continua...
+                } else{                                             
+                    System.out.println("Prato não cadastrado ou não existe.");
+                    terminal.limpar(2000);                                      // Senão, Informa e não continua
+                    break;
+                }
 
                 String sql = "UPDATE prato SET nome = ? WHERE nome = ?";
 
@@ -59,47 +69,64 @@ public class Admin {
                     e.printStackTrace();
                 }
                 break;
-            case 2:
+            case 2: // Mudar Valor de um Prato
                 System.out.printf("Prato no qual deseja mudar o preço: ");
-                String Prato = leitor.nextLine();
+                nomePrato = leitor.nextLine();
                 System.out.printf("Novo preço do prato: ");
                 String NovoPreco = leitor.nextLine();
 
-                sql = "UPDATE prato SET valor = ? WHERE nome = ?";
+                if (VerificarPrato(nomePrato)) {            // Verifica se o Prato Existe
+                    // Se sim, continua...
+                }else{
+                    System.out.println("Prato não cadastrado ou não existe.");
+                    terminal.limpar(2000);                                      // Senão, Informa e não continua
+                    break;
+                }
+
+                sql = "UPDATE prato SET valor = ? WHERE nome = ?";                  // Query para mudar preço de um Prato
 
                 try (Connection conexao = conectiondb.conectar();
                     PreparedStatement stmt = conexao.prepareStatement(sql)){
     
                     stmt.setString(1, NovoPreco);
-                    stmt.setString(2, Prato);
+                    stmt.setString(2, nomePrato);
     
                     int LinhasAfetadas = stmt.executeUpdate();
-                    System.out.println("Preço do Prato " + Prato +" atualizado para R$" + NovoPreco +".");
+                    System.out.println("Preço do Prato " + nomePrato +" atualizado para R$" + NovoPreco +".");
                     System.out.println("Linhas afetadas: "+ LinhasAfetadas);
                 }catch(SQLException e){
                     e.printStackTrace();
                 }
                 break;
-            case 3:
+            case 3: // Mudar Disponibilidade de um Prato
                 System.out.printf("Prato no qual deseja mudar a disponibilidade: ");
-                Prato = leitor.nextLine();
+                nomePrato = leitor.nextLine();                          // Armazena o Prato a ser atualizado
+
+                if (VerificarPrato(nomePrato) == true) {                // Verifica se o Prato Existe
+                    // Se sim, continua...
+                }else{                                              
+                    System.out.println("Prato não existe ou não cadastrado.");
+                    terminal.limpar(2000);                                      // Senão, informa e não continua
+                    break;
+                }
 
                 System.out.printf("Disponivel('1') ou Indisponivel('0'): ");
-                int disponibilidade = leitor.nextInt();
+                int disponibilidade = leitor.nextInt();                     // armazena opção de disponibilidade
+                leitor.nextLine();                                          // Limpa o Buffer
 
-                sql = "UPDATE prato SET disponivel = ? WHERE nome = ?";
+                sql = "UPDATE prato SET disponivel = ? WHERE nome = ?";             // Query para Atualizar disponibilidade
 
                 if (disponibilidade == 1) {
                     try (Connection conexao = conectiondb.conectar();
                     PreparedStatement stmt = conexao.prepareStatement(sql)){
     
-                    stmt.setInt(1, disponibilidade);
-                    stmt.setString(2, Prato);
+                    stmt.setString(1, "sim");
+                    stmt.setString(2, nomePrato);
     
                     int LinhasAfetadas = stmt.executeUpdate();
-                    System.out.println("Disponibilidade do Prato "+ Prato +" atualizado para Disponivel.");
+                    System.out.println("Disponibilidade do Prato "+ nomePrato +" atualizado para Disponivel.");
                     System.out.println("Linhas afetadas: "+ LinhasAfetadas);
-                    terminal.limpar(2000);
+                    terminal.limpar(3000);
 
                     }catch(SQLException e){
                     e.printStackTrace();
@@ -108,13 +135,13 @@ public class Admin {
                     try (Connection conexao = conectiondb.conectar();
                     PreparedStatement stmt = conexao.prepareStatement(sql)){
     
-                    stmt.setInt(1, disponibilidade);
-                    stmt.setString(2, Prato);
+                    stmt.setString(1, "nao");
+                    stmt.setString(2, nomePrato);
     
                     int LinhasAfetadas = stmt.executeUpdate();
-                    System.out.println("Disponibilidade do Prato "+ Prato +" atualizado para Indisponivel.");
+                    System.out.println("Disponibilidade do Prato "+ nomePrato +" atualizado para Indisponivel.");
                     System.out.println("Linhas afetadas: "+ LinhasAfetadas);
-                    terminal.limpar(2000);
+                    terminal.limpar(3000);
 
                     }catch(SQLException e){
                     e.printStackTrace();
@@ -142,6 +169,49 @@ public class Admin {
             int LinhasAfetadas = stmt.executeUpdate();
             System.out.println("Prato "+ nomePrato +" Deletado.");
             System.out.println("Linhas Afetadas: " + LinhasAfetadas);
+            
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean VerificarPrato(String nomePrato){
+        String sql = "SELECT COUNT(*) FROM prato WHERE nome = ?";
+
+        try (Connection conexao = conectiondb.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql)){
+
+            stmt.setString(1, nomePrato);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                if (count > 0) {
+                    return true;
+                } else{
+                    return false;
+                }
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void PratosCadastrados(){
+        String sql = "SELECT nome, disponivel FROM prato";
+
+        try (Connection conexao = conectiondb.conectar();
+            PreparedStatement stmt = conexao.prepareStatement(sql)){
+            
+            ResultSet rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                String prato = rs.getString("nome");
+                String disponivel = rs.getString("disponivel");
+                System.out.println("Prato: "+prato+"\nDisponivel: "+disponivel+"\n");
+            }
             
         }catch (SQLException e) {
             e.printStackTrace();
