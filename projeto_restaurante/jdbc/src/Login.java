53356ab98c4cd7533163dbaea39ba5660b6d858e
Ricardo Silva Flores <purpleknight170@gmail.com>
2024-05-16 13:19:06 -0300

    metodo inserir garçom no banco

--- projeto_restaurante/jdbc/src/Login.java
+++ projeto_restaurante/jdbc/src/Login.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.sql.*;
 
 // Classe para Fazer Login como Garçom ou Administrador
 public class Login {
@@ -19,18 +20,59 @@ public class Login {
 
     // Definindo credenciais de Login para Administrador
     public Login(){
-        credenciaisAdmin.put("admin", "admin123");                // Definindo um Usuário e Senha para Admin
+        credenciaisAdmin.put("admin", "admin123");            // Definindo um Usuário e Senha para Admin
     }
 
-    // Criando novo Garçom
-    public void CriarGarcom(String GarcomUsuario, String GarcomSenha){
-        credenciaisGarcom.put(GarcomUsuario, GarcomSenha);
-    }
+    public void CriarGarcom (String nomeGarcom, int idadeGarcom, String usuarioGarcom, String senhaGarcom){
+        String sqlGarcom = "INSERT INTO garcom (nome, idade) VALUES (?, ?)";
+        String sqlLogin = "INSERT INTO login (id_garcom, usuario, senha) VALUES (?, ?, ?)";
 
+        try (Connection conexao = conectiondb.conectar();
+        PreparedStatement stmt1 = conexao.prepareStatement(sqlGarcom, Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement stmt2 = conexao.prepareStatement(sqlLogin)){
+
+            // Inserindo informações do Garçom através da variável "stmt1" armazenando a função
+            stmt1.setString(1, nomeGarcom);
+            stmt1.setInt(2, idadeGarcom);
+            int LinhasAfetadas1 = stmt1.executeUpdate();
+
+            // Obter ID do Garçom
+            int idGarcomInserido;
+            try(ResultSet generetedKeys = stmt1.getGeneratedKeys()){
+                if (generetedKeys.next()) {
+                    idGarcomInserido = generetedKeys.getInt(1);
+                } else {
+                    throw new SQLException("Falha ao obter o ID do Garçom inserido.");
+                }
+            }
+
+            // Inserindo Dados na tabela Login
+            stmt2.setInt(1, idGarcomInserido);
+            stmt2.setString(2, usuarioGarcom);
+            stmt2.setString(3, senhaGarcom);
+
+            credenciaisGarcom.put(usuarioGarcom, senhaGarcom);         // Criando novo Login para Garçom
+
+            int LinhasAfetadas2 = stmt2.executeUpdate();
+
+            System.out.println("Linhas Afetadas Tabela Garçom: "+ LinhasAfetadas1);
+            System.out.println("Linhas Afetadas Tabela Login: "+ LinhasAfetadas2);
+
+            stmt1.close();
+            stmt2.close();
+            conexao.close();
+
+            System.out.println("Cadastro Feito!");
+
+        } catch (SQLException e){
+            System.out.println("Erro ao inserir dados: "+ e.getMessage());
+        }
+    }    
+    
     // Classe para Realizar a Verificação de Login do Usuário
-    public boolean LoginGarcom(String GarcomUsuario, String GarcomSenha){
+    public boolean LoginGarcom(String usuarioGarcom, String senhaGarcom){
         // Verifica se a entrada do Usuário se iguala ao Usuário e Senha definidos na classe e Retorna True ou False
-        if (credenciaisGarcom.containsKey(GarcomUsuario) && credenciaisGarcom.get(GarcomUsuario).equals(GarcomSenha)) {
+        if (credenciaisGarcom.containsKey(usuarioGarcom) && credenciaisGarcom.get(usuarioGarcom).equals(senhaGarcom)) {
             return true;
         }else{
             return false;
