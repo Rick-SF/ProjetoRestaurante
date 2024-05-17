c181e044ada28ff7ff4d8390eb6dd5dd5bb215ae
Ricardo Silva Flores <purpleknight170@gmail.com>
2024-05-17 18:51:25 -0300

    classe garcom

---
+++ projeto_restaurante/jdbc/src/Garcom.java
@@ -0,0 +1,46 @@
+public class Garcom {
+    private String nome;
+    private int idade;
+    private String usuarioGarcom;
+    private String senhaGarcom;
+
+    // Construtor
+    public Garcom(String nome, int idade, String usuarioGarcom, String senhaGarcom) {
+        this.nome = nome;
+        this.idade = idade;
+        this.usuarioGarcom = usuarioGarcom;
+        this.senhaGarcom = senhaGarcom;
+    }
+
+    public String getUsuarioGarcom() {
+        return usuarioGarcom;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setUsuarioGarcom(String usuarioGarcom) {
+        this.usuarioGarcom = usuarioGarcom;
+    }
+
+    public void setSenhaGarcom(String senhaGarcom) {
+        this.senhaGarcom = senhaGarcom;
+    }
+
+    public String getSenhaGarcom() {
+        return senhaGarcom;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
